1ae056f4b6e5034e584e8fe5ea332b87fdb176ea
duyguadadivle <duyguadadivle@gmail.com>
2022-04-12 20:40:45 +0300

    RemainderYear

---
+++ src/RemainderYear.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class RemainderYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int year, remainder;
+        System.out.println("Yıl Giriniz: ");
+        year = input.nextInt();
+
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    System.out.println(year + "bir artık yıldır!");
+                }
+                else {
+                    System.out.println(year + "bir artık yıldır değildir!");
+                }
+            }
+            else{
+                System.out.println(year + "bir artık yıldır!");
+            }
+        }
+        else{
+            System.out.println(year + "bir artık yıldır değildir!");
+        }
+
+    }
+}
+
+/*
+
+Genel bir kural olarak,
+artık yıllar 4 rakamının katı olan yıllardır:
+
+1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+100'ün katı olan yıllardan sadece
+400'e kalansız olarak bölünebilenler artık yıldır:
+
+Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.
+Sadece 400'e tam olarak bölünebilenlerin artık yıl kabul edilmesinin
+nedeni, bir astronomik yılın 365,25 gün değil,
+yaklaşık olarak 365,242 gün olmasından kaynaklanan hatayı gidermektir.
+*/
