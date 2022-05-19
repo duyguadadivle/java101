f2bd6b9ca4a0dd634e9ad579e212830670439bb0
duyguadadivle <duyguadadivle@gmail.com>
2022-05-19 14:25:39 +0300

    perfect number

---
+++ src/PerfectNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number, sum=0;
+        System.out.println("Bir sayı giriniz:");
+        number = input.nextInt();
+        for(int i=1; i<number; i++){
+            if(number%i == 0){
+                sum += i;
+            }
+        }
+        
+        if(number == sum){
+            System.out.println(number + "Mükemmel sayıdır.");
+        }
+        else {
+            System.out.println(number + "Mükemmel sayı değildir.");
+        }
+
+    }
+}
