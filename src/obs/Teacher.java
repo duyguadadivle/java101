e3b3830e63f0ac0b8ccd4cfa2e5f088eb8967f2b
duyguadadivle <duyguadadivle@gmail.com>
2023-05-29 15:20:59 +0300

    obs

---
+++ src/obs/Teacher.java
@@ -0,0 +1,18 @@
+package obs;
+
+public class Teacher {
+
+    //Nitelikler : name,mpno,branch
+    //Metotlar : Teacher()
+
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+
+}
