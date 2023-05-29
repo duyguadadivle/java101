60b52fa0606354844a85cb4118f7840eac908cd2
duyguadadivle <duyguadadivle@gmail.com>
2023-05-29 17:51:28 +0300

    harmonic average arrays

---
+++ src/HarmonicAverageArrays.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class HarmonicAverageArrays {
+    public static void main(String[] args) {
+        
+        double[] numberList = {2,4,6,8};
+        double sum = 0;
+
+        for(double item : numberList){
+            sum += 1 / item;
+        }
+
+        System.out.println(Arrays.toString(numberList) + "Dizinin");
+        System.out.println("Harmonic Ortalama: " + numberList.length / sum);
+
+    }
+}
